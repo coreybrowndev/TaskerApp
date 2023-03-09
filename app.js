a028af0823b8d1ed121f23a2942bc698e46880ea
Corey <corey.brown@bsu.edu>
2023-03-08 20:02:18 -0500

    Add functionality to add tasks to completed section

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 
+const form = document.getElementById('form');
+const input = document.getElementById('add_task');
+const tasks = document.getElementById('tasks');
+const currentCompletedCounter = document.querySelector("#curr_count");
+const err = document.querySelector('.err-msg');
+const taskList = document.getElementById('task-list');
+const completedOptions = document.getElementById('options');
+const checkbox = document.querySelectorAll("input[name=tasks]");
+const taskParent = document.querySelector("#tasks");
+let completedCounter = 0;
+
+
 
 document.addEventListener('click', e => {
+
     const isDropDownButton = e.target.matches("[data-dropdown-button]")
+
     if(!isDropDownButton && e.target.closest('[data-dropdown]') != null) return
 
     let currentDropDown;
@@ -18,46 +32,28 @@ document.addEventListener('click', e => {
 });
 
 
-let form = document.getElementById('form');
-let input = document.getElementById('add_task');
-let tasks = document.getElementById('tasks');
-let err = document.querySelector('.err-msg');
-let taskList = document.getElementById('task-list');
-
-
 //event listener which allows sumbit button to be pressed
 form.addEventListener('submit', e => {
     e.preventDefault();
-    console.log("button-clicked");
 
     formValidation();
-
 });
 
 
 let formValidation = () => {
     if(input.value === "") {
-        err.innerHTML = "Woah! Task can't be empty";
-        console.log("failure");
+        err.innerHTML = "Task can't be empty";
     }else {
-        acceptData();
+        newPost();
         input.value = "";
     }
-}
-
-let data = {};
-
-let acceptData = () => {
-    data["task"] = input.value;
-    
-    newPost();
 }
 
 let newPost = () => {
     tasks.innerHTML += `<li>
     <div>
-        <input type="checkbox" name="tasks" id="1">
-        <span>${data.task}</span>
+        <input type="checkbox" name="tasks">
+        <span>${input.value}</span>
     </div>
 
     <div>
@@ -69,8 +65,9 @@ let newPost = () => {
             <i class="fa-solid fa-trash-can"></i>
         </button>
     </div>
-</li>`;q
+</li>`
 }
+
 
 let deleteTask = (e) => {
     e.parentElement.parentElement.remove();
@@ -81,9 +78,45 @@ let editTask = (e) => {
     e.parentElement.parentElement.remove();
 }
 
+//add checkbox to completed section after transition finishes
+taskParent.addEventListener("transitionend", (e) => {
+    if(e.target.nodeName === "INPUT") {
+        e.target.parentElement.parentElement.remove();
+        completedCounter++;
+        currentCompletedCounter.innerHTML = completedCounter;
+        completedOptions.innerHTML += `<li>
+        <div>
+            <input class="task-el" type="checkbox"  id="completed">
+            <span>${e.target.nextElementSibling.innerHTML}</span>
+        </div>
+
+        <div>
+            <button title="remove-task" onclick="deleteTask(this)" class="remove-task">
+                <i class="fa-solid fa-trash-can"></i>
+            </button>
+        </div>
+    </li>`
+
+    }
+});
 
 
-const span = document.getElementById("curr_count").innerHTML = 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
